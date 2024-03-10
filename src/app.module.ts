7056ace4c96d7c7061cef89c653f4fd4deb23b62
Thắng Nguyễn <128893050+winncoder@users.noreply.github.com>
2024-03-10 15:14:04 +0700

    Ot1-48 add husky and commitlint (#2)
    * feat/OT-28: Login with GG
    
    * feat/OT: setup husky, add abstract entity and enum
    
    * OT:  husky, add abstract entity and enum
    
    * test husky
    
    * test husky when i commitlint/cli
    
    * test husky when i commitlint/cli save dev
    
    * Set up Husky and commitlint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './common/db/db.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DbModule,
+    AuthModule,
+    PassportModule.register({ session: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
