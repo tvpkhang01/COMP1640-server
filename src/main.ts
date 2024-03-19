5c8a5106f32ef6894cd1f562d990cef6b7b92e94
winncoder <thangtnsa000@gmail.com>
2024-03-19 10:21:35 +0700

    feat(OT1-48): add to update and delete user

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -7,11 +7,13 @@ import {
   Param,
   Delete,
   Query,
+  ValidationPipe,
+  Patch,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { GetUserParams } from './dto/getList_user.dto';
-// import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -26,9 +28,10 @@ export class UserController {
   async findAll(@Query() params: GetUserParams) {
     return this.userService.getUsers(params);
   }
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+
+  @Get(':ID')
+  async findOne(@Param('ID') ID: string) {
+    return this.userService.getUserById(ID);
   }
 
   // @Patch(':id')
@@ -36,8 +39,26 @@ export class UserController {
   //   return this.userService.update(+id, updateUserDto);
   // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @Patch(':ID')
+  async update(
+    @Param('ID') ID: string,
+    @Body(new ValidationPipe()) updateUserDto: UpdateUserDto,
+  ) {
+    const result = await this.userService.update(ID, updateUserDto);
+    return { result, message: 'Successfully update user' };
+  }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.userService.remove(+id);
+  // }
+  @Delete(':ID')
+  async remove(@Param('ID') ID: string) {
+    const result = await this.userService.remove(ID);
+    if (result.message) {
+      return { message: result.message };
+    } else {
+      return { data: result.data, message: 'Success' };
+    }
   }
 }
