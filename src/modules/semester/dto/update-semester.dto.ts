79714dc8f9b25bdaf29c7fc00e93fc2c9d0b3980
HoangLoc <huynhngochoangloc0211102@gmail.com>
2024-03-19 15:45:12 +0700

    feat(OT1-49): validate semester

--- src/modules/semester/dto/update-semester.dto.ts
+++ src/modules/semester/dto/update-semester.dto.ts
@@ -1,8 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSemesterDto } from './create-semester.dto';
 
-export class UpdateSemesterDto extends PartialType(CreateSemesterDto) {
-  SemesterName: string;
-  StartDate: Date;
-  EndDate: Date;
-}
+export class UpdateSemesterDto extends PartialType(CreateSemesterDto) {}
