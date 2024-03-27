df52daf739f65972c8c493a174b55b24e4755218
HoangLoc <huynhngochoangloc0211102@gmail.com>
2024-03-27 14:54:35 +0700

    feat(OT1-78): add getList contribution comment

---
+++ src/modules/comtributionComment/dto/getList-contributionComment.dto.ts
@@ -0,0 +1,8 @@
+import { PageOptionsDto } from 'src/common/dtos/pageOption';
+
+export class GetContributionCommentParams extends PageOptionsDto {
+  contributionId: string;
+  coordinatorId: string;
+  comment: string;
+  commentDate: Date;
+}
