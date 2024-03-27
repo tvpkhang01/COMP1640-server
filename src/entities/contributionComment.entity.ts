2f6d8e3296886e624f2cf516d271d1c318e6a15e
HoangLoc <huynhngochoangloc0211102@gmail.com>
2024-03-27 11:57:32 +0700

    feat(OT1-78): setup folder contributionComment

---
+++ src/entities/contributionComment.entity.ts
@@ -0,0 +1,25 @@
+import { AbstractEntity } from 'src/common/entities';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class ContributionComment extends AbstractEntity {
+  @PrimaryGeneratedColumn('uuid')
+  ID: string;
+
+  @Column()
+  contributionId: string;
+
+  @Column()
+  coordinatorId: string;
+
+  @Column()
+  comment: string;
+
+  @Column()
+  commentDate: Date;
+
+  constructor(contributionComment: Partial<ContributionComment>) {
+    super();
+    Object.assign(this, contributionComment);
+  }
+}
