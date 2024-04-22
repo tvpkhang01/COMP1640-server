94471d25abc7b4ad4d3f21349d249ece2c441736
Trương Văn Phúc Khang <phuckhang01@gmail.com>
2024-04-22 22:38:04 +0700

    fix(deployment): fix more things

--- src/modules/comtributionComment/dto/update-contributionComment.dto.ts
+++ src/modules/comtributionComment/dto/update-contributionComment.dto.ts
@@ -1,5 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateContributionCommentDto } from './create-ContributionComment.dto';
+import { CreateContributionCommentDto } from '../dto/create-contributionComment.dto';
 
 export class UpdateContributionCommentDto extends PartialType(
   CreateContributionCommentDto,
